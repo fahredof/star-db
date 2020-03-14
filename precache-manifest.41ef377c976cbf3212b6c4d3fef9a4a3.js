756e8fefba830db70615bf68c73f04b2584b4440
fahredof <turik20121998@gmail.com>
2020-03-14 20:04:05 +0300

    Updates

---
+++ precache-manifest.41ef377c976cbf3212b6c4d3fef9a4a3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d3459c3582c129fa68fdc2779d0334fd",
+    "url": "/star-db/index.html"
+  },
+  {
+    "revision": "c73f8ebadadc92706de8",
+    "url": "/star-db/static/css/main.431d0434.chunk.css"
+  },
+  {
+    "revision": "9c7bd0f415c873a3f76c",
+    "url": "/star-db/static/js/2.b82a8f66.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/star-db/static/js/2.b82a8f66.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c73f8ebadadc92706de8",
+    "url": "/star-db/static/js/main.52201d57.chunk.js"
+  },
+  {
+    "revision": "7417beba386f58d3e97b",
+    "url": "/star-db/static/js/runtime-main.bb006c70.js"
+  },
+  {
+    "revision": "83f8271fe2c15f31efffd74e9c685e1e",
+    "url": "/star-db/static/media/death-star-icon.83f8271f.png"
+  }
+]);
